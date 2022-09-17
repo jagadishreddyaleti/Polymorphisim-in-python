5254452e4d100ccb3b6112f2c8ae657f94e9890e
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-17 22:42:55 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,19 @@
+# class animal():
+#     def noise(self):
+#         pass
+# class cat(animal):
+#     def noise(self):
+#         print('meow')
+# class dog(animal):
+#     def noise(self):
+#         print('boww')
+# class donkey(animal):
+#     def noise(self):
+#         print('haaaan') 
+
+# a=cat()
+# a.noise()   
+# b=dog()
+# b.noise() 
+# c=donkey()
+# c.noise()                       
